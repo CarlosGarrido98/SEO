84757a6ee55abb6f61ae9f4ecd606ef83f05a483
Carlos Garrido <129065902+CarlosGarrido98@users.noreply.github.com>
2026-01-28 19:02:06 +0100

    Create sitemaps

---
+++ Proyecto_SEO/EjemploSitemaps/sitemap.php
@@ -0,0 +1,80 @@
+<main>
+    <div class="sitemapcont">
+        <h1>
+            Mapa del sitio
+        </h1>
+        <div class="links">
+            <div class="column">
+                <h2>Productos</h2>
+                <div>
+                    <ul class="links-ul">
+                        <li><a href="/products/mtg">Magic: The Gathering</a></li>
+                        <li><a href="/products/lor">Lorcana</a></li>
+                        <li><a href="/products/ygo">Yu-Gi-Oh!</a></li>
+                        <li><a href="/products/pok">Pokémon</a></li>
+                        <li><a href="/products/op">One Piece TCG</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <h2>Usuarios</h2>
+                <div>
+                    <ul class="links-ul">
+                        <li><a href="/signup">Regístrate</a></li>
+                        <li><a href="/login">Inicia sesión</a></li>
+                        <li><a href="/mypage">Mi cuenta</a></li>
+                        <li><a href="/mypage">Mis pedidos</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <h2>Sobre Arcadia</h2>
+                <div>
+                    <ul class="links-ul">
+                        <li><a href="/contact">Nosotros</a></li>
+                        <li><a href="/contact">Contacto</a></li>
+                        <li><a href="/terms">Términos y condiciones</a></li>
+                        <li><a href="/privacy">Política de privacidad</a></li>
+                        <li><a href="/cookies">Política de cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <h2>Aprende a jugar</h2>
+                <div>
+                    <ul class="links-ul">
+                        <li><a href="/learn-mtg">Magic: The Gathering</a></li>
+                        <li><a href="/learn-lor">Lorcana</a></li>
+                        <li><a href="/learn-ygo">Yu-Gi-Oh!</a></li>
+                        <li><a href="/learn-pok">Pokémon</a></li>
+                        <li><a href="/learn-op">One Piece TCG</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <h2>Eventos</h2>
+                <div>
+                    <ul class="links-ul">
+                        <li><a href="/calendar">Calendario de eventos</a></li>
+                        <li><a href="/calendar">Eventos próximos</a></li>
+                        <li><a href="/calendar">Noticias</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <h2>Nuestras Redes</h2>
+                <div class="column-link">
+                    <ul class="column-link">
+                        <li><a href="https://www.facebook.com/Arcadia-Online-Store-108096711336637/" target="_blank"> <i class="bi bi-facebook link"></i></a></li>
+                        <li><a href="https://twitter.com/ArcadiaOnlineStore" target="_blank"><i class="bi bi-twitter-x link"></i></a></li>
+                        <li><a href="https://www.instagram.com/arcadia_online_store/" target="_blank"><i class="bi bi-instagram link"></i></a></li>
+                        <li><a href="https://discord.gg/v8z9gT4" target="_blank"><i class="bi bi-discord link"></i></a></li>
+                        <li><a href="https://tiktok.com"><i class="bi bi-tiktok link"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+
+</main>
